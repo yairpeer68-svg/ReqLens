21397f6950ab1b6e5936ef50329fa668f4b46d11
yairpeer68-svg <yairpeer68-svg@users.noreply.github.com>
2026-09-06 12:47:04 +0300

    Fix hevtunnel TProxyService package

--- app/src/main/java/com/reqlens/app/HevTun2SocksBackend.java
+++ app/src/main/java/com/reqlens/app/HevTun2SocksBackend.java
@@ -7,7 +7,7 @@ import android.net.Network;
 import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
 
-import com.zaneschepke.hevtunnel.TProxyService;
+import org.amnezia.awg.hevtunnel.TProxyService;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -26,7 +26,7 @@ public final class HevTun2SocksBackend implements ForwardingBackend {
 
     @Override public boolean isAvailable() {
         try {
-            Class.forName("com.zaneschepke.hevtunnel.TProxyService", true, HevTun2SocksBackend.class.getClassLoader());
+            Class.forName("org.amnezia.awg.hevtunnel.TProxyService", true, HevTun2SocksBackend.class.getClassLoader());
             availabilityError = "";
             return true;
         } catch (Throwable t) {
